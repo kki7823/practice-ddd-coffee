4e6c299dcd6b316832f739b4b31447b2ba8ff848
kki7823 <kjd55399997@gmail.com>
2023-05-17 00:34:05 +0900

    feat: OrderStatus 정의

--- src/main/java/com/kikim/practice/coffee/order/OrderState.java
+++ src/main/java/com/kikim/practice/coffee/order/OrderState.java
@@ -1,5 +1,10 @@
 package com.kikim.practice.coffee.order;
 
-public class OrderState {
-
+public enum OrderState {
+    ORDER_WATING,
+    CANCLED,
+    PAYMENT_WAITNG,
+    PREPARING,
+    READY,
+    FAIL,
 }
